d52ac919cb1a2f3ddc1beaecd45eaeff977602a3
thebigthing313 <adriankabigting@gmail.com>
2025-12-30 16:50:05 -0500

    feat: add initial configuration files for login app including components, package, PostCSS, TypeScript, and Vite

---
+++ apps/login/vite.config.ts
@@ -0,0 +1,18 @@
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+	server: {
+		port: 3000,
+		strictPort: true,
+		allowedHosts: ['login.local.test']
+	},
+	plugins: [
+		tsConfigPaths(),
+		tanstackStart(),
+		// react's vite plugin must come after start's vite plugin
+		viteReact(),
+	],
+});
