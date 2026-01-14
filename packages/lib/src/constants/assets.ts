dfd3483f48cab1de8c7054ed0e79b5ce29de149b
thebigthing313 <adriankabigting@gmail.com>
2026-01-14 13:50:27 -0500

    style: sorted objects and attributes

--- packages/lib/src/constants/assets.ts
+++ packages/lib/src/constants/assets.ts
@@ -1,11 +1,11 @@
 export const ASSET_URLS = {
-	notFoundImage:
-		"https://qkgqhexcnbavdpmxzcii.supabase.co/storage/v1/object/public/assets/404-not-found.png",
+	favicon:
+		"https://qkgqhexcnbavdpmxzcii.supabase.co/storage/v1/object/public/assets/favicon.ico",
 	logo: "https://qkgqhexcnbavdpmxzcii.supabase.co/storage/v1/object/public/assets/logo.png",
 	logo192:
 		"https://qkgqhexcnbavdpmxzcii.supabase.co/storage/v1/object/public/assets/logo192.png",
 	logo512:
 		"https://qkgqhexcnbavdpmxzcii.supabase.co/storage/v1/object/public/assets/logo512.png",
-	favicon:
-		"https://qkgqhexcnbavdpmxzcii.supabase.co/storage/v1/object/public/assets/favicon.ico",
+	notFoundImage:
+		"https://qkgqhexcnbavdpmxzcii.supabase.co/storage/v1/object/public/assets/404-not-found.png",
 };
