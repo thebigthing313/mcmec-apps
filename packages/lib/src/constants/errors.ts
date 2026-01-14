da00e0633f635103438a1988d19ffe3edc640ccb
thebigthing313 <adriankabigting@gmail.com>
2026-01-14 13:49:35 -0500

    feat: add crud errors

--- packages/lib/src/constants/errors.ts
+++ packages/lib/src/constants/errors.ts
@@ -1,32 +1,38 @@
 export const ErrorMessages = {
 	AUTH: {
-		UNAUTHORIZED: "You must be logged in to access this resource.",
 		FORBIDDEN: "You do not have permission to this action or resource.",
-		SESSION_EXPIRED: "Your session has expired. Please log in again.",
 		INVALID_JWT: "The provided authentication token is invalid.",
-		UNABLE_TO_FETCH_CLAIMS: "Unable to fetch user claims. Please try again.",
 		NOT_ONBOARDED: "User has not properly onboarded.",
+		SESSION_EXPIRED: "Your session has expired. Please log in again.",
+		UNABLE_TO_FETCH_CLAIMS: "Unable to fetch user claims. Please try again.",
 		UNABLE_TO_RETRIEVE_SESSION: "Unable to retrieve session. Please try again.",
-	},
-	VALIDATION: {
-		INVALID_EMAIL: "Please enter a valid email address.",
-		INVALID_UUID: "The provided ID is not valid.",
-		PASSWORD_TOO_SHORT: "Password must be at least 8 characters long.",
-	},
-	SERVER: {
-		INTERNAL_ERROR: "An unexpected error occurred. Please try again later.",
-		SERVICE_UNAVAILABLE: "The service is temporarily down.",
-		ENVIRONMENT_MISCONFIGURED: "The server environment is misconfigured.",
+		UNAUTHORIZED: "You must be logged in to access this resource.",
 	},
 	BROWSER: {
 		ROOT_ELEMENT_NOT_FOUND: "Root element not found",
 	},
 	DATABASE: {
-		UNABLE_TO_FETCH: (table: string) =>
-			`Unable to fetch data from the ${table} table.`,
+		UNABLE_TO_DELETE: (table: string, dbError?: string) =>
+			`Unable to delete data from the ${table} table.${dbError ? ` Error: ${dbError}` : ""}`,
+		UNABLE_TO_FETCH: (table: string, dbError?: string) =>
+			`Unable to fetch data from the ${table} table.${dbError ? ` Error: ${dbError}` : ""}`,
+		UNABLE_TO_INSERT: (table: string, dbError?: string) =>
+			`Unable to insert data into the ${table} table.${dbError ? ` Error: ${dbError}` : ""}`,
+		UNABLE_TO_UPDATE: (table: string, dbError?: string) =>
+			`Unable to update data in the ${table} table.${dbError ? ` Error: ${dbError}` : ""}`,
+	},
+	SERVER: {
+		ENVIRONMENT_MISCONFIGURED: "The server environment is misconfigured.",
+		INTERNAL_ERROR: "An unexpected error occurred. Please try again later.",
+		SERVICE_UNAVAILABLE: "The service is temporarily down.",
 	},
 	UI: {
 		FAILED_TO_COPY: "Failed to copy to clipboard. Please try again.",
+	},
+	VALIDATION: {
+		INVALID_EMAIL: "Please enter a valid email address.",
+		INVALID_UUID: "The provided ID is not valid.",
+		PASSWORD_TOO_SHORT: "Password must be at least 8 characters long.",
 	},
 } as const;
 
