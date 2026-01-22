4ddcb61e5e0f49b051d082707a8db5311cafbfe7
thebigthing313 <adriankabigting@gmail.com>
2026-01-22 14:54:42 -0500

    feat: add location, notice_url, and report_url columns to meetings table

---
+++ supabase/migrations/20260122195417_meetings_other_fields.sql
@@ -0,0 +1,7 @@
+alter table "public"."meetings" add column "location" text not null;
+
+alter table "public"."meetings" add column "notice_url" text;
+
+alter table "public"."meetings" add column "report_url" text;
+
+
