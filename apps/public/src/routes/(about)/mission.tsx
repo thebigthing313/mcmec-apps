b4d30f9a6f2592629a2e4c20677f9b0bbb2671c2
thebigthing313 <adriankabigting@gmail.com>
2026-01-30 11:38:45 -0500

    feat: enhance mission page with additional content and image

--- apps/public/src/routes/(about)/mission.tsx
+++ apps/public/src/routes/(about)/mission.tsx
@@ -7,9 +7,14 @@ export const Route = createFileRoute("/(about)/mission")({
 function RouteComponent() {
 	return (
 		<div className="mx-auto w-full max-w-7xl p-4">
-			<article className="prose lg:prose-xl">
+			<article className="prose lg:prose-xl max-w-none">
 				<h1>Mission Statement</h1>
 				<p>
+					<img
+						alt="MCMEC Building"
+						className="mb-4 w-full md:float-right md:ml-6 md:w-1/2"
+						src="/shared/building.webp"
+					/>
 					The Middlesex County Mosquito Extermination Commission (MCMEC) was
 					created in 1914. In accordance with the laws of the State of New
 					Jersey, N.J.S.A, Title 26, Chapter 9 and 26:3-46 et. seq., the
@@ -28,6 +33,15 @@ function RouteComponent() {
 						Nile virus (WNV).
 					</li>
 				</ul>
+				<p>
+					The Commission recognizes mosquito-borne diseases to be hazardous to
+					the health and safety of the people of Middlesex County and is
+					committed to reducing the chance of infection through a comprehensive
+					approach to mosquito control. This approach in in accordance with
+					scientifically based best management practices and includes mosquito
+					surveillance, water management, biological control, chemical control
+					and public education.
+				</p>
 			</article>
 		</div>
 	);
