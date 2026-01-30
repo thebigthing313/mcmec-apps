0542e08d2b928e95b39bb7430f2344eb0f98b286
thebigthing313 <adriankabigting@gmail.com>
2026-01-30 11:38:37 -0500

    feat: add leadership route and component with commissioner details to About section

--- apps/public/src/routes/(about)/leadership-and-staff.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/(about)/leadership-and-staff')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/(about)/leadership-and-staff"!</div>
-}
