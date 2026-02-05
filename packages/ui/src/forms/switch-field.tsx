e52f85d7b445d7c06e4c38e924db942f69968a6c
thebigthing313 <adriankabigting@gmail.com>
2026-02-05 16:14:14 -0500

    feat: enhance SwitchField component with label support for true/false states

--- packages/ui/src/forms/switch-field.tsx
+++ packages/ui/src/forms/switch-field.tsx
@@ -2,12 +2,20 @@ import { FormField } from "../blocks/form-field";
 import { Switch } from "../components/switch";
 import { useFieldContext } from "./form-context";
 
+interface SwitchFieldProps {
+	labelWhenTrue?: string;
+	labelWhenFalse?: string;
+}
+
 export function SwitchField({
+	labelWhenTrue,
+	labelWhenFalse,
 	...formFieldProps
-}: Omit<
-	React.ComponentPropsWithRef<typeof FormField>,
-	"children" | "errors" | "htmlFor" | "data-invalid"
->) {
+}: SwitchFieldProps &
+	Omit<
+		React.ComponentPropsWithRef<typeof FormField>,
+		"children" | "errors" | "htmlFor" | "data-invalid"
+	>) {
 	const field = useFieldContext<boolean>();
 	return (
 		<FormField
@@ -17,7 +25,14 @@ export function SwitchField({
 			orientation="horizontal"
 			{...formFieldProps}
 		>
-			<div className="flex items-center space-x-2">
+			<div className="flex w-full flex-row items-start justify-between gap-4 rounded-md bg-accent/10 p-4">
+				{labelWhenTrue || labelWhenFalse ? (
+					<div className="text-wrap font-semibold">
+						{field.state.value
+							? labelWhenTrue && <span>{labelWhenTrue}</span>
+							: labelWhenFalse && <span>{labelWhenFalse}</span>}
+					</div>
+				) : null}
 				<Switch
 					aria-invalid={!field.state.meta.isValid}
 					checked={field.state.value}
